b5d825f77970d03e807cebac232562e5183a2a9c
bweatherson <brian@weatherson.org>
2024-09-05 14:39:31 -0400

    Made the autobib

---
+++ make-autobib.R
@@ -0,0 +1,15 @@
+bibtexgen <- philo_bib_fix |>
+  select(BIBTEXKEY = id, YEAR = year, JOURNAL = journal, end_of_longcite, AUTHOR = auth, TITLE = art_title) |>
+  filter(grepl("[0-9]$", end_of_longcite)) |>
+  filter(grepl("^[0-9]", end_of_longcite)) |>
+  filter(str_length(end_of_longcite) >= 8) |>
+  arrange(end_of_longcite) |>
+  separate_wider_delim(end_of_longcite, names = c("VOLUME", "NUMBER", "PAGES"), delim = " ", too_few = "align_start", too_many = "drop") |>
+  filter(!is.na(PAGES)) |>
+  filter(grepl("[0-9]$", PAGES)) |>
+  filter(grepl("[0-9]$", VOLUME)) |>
+  filter(grepl("^\\(", NUMBER)) |>
+  mutate(NUMBER = str_sub(NUMBER, start = 2, end = str_length(NUMBER) - 2)) |>
+  mutate(CATEGORY = "ARTICLE")
+
+df2bib(bibtexgen, file = "autobib.bib")
