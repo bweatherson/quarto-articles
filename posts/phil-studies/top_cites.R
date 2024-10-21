0acbbeb8394c2f4afb935e0e42e9ec2b3bf82416
bweatherson <brian@weatherson.org>
2024-10-21 17:36:45 -0400

    Oct 21

---
+++ posts/phil-studies/top_cites.R
@@ -0,0 +1,33 @@
+require(tidyverse)
+
+citation_count <- active_cites |>
+  group_by(old) |>
+  tally(name = "cites")
+
+short_bib <- philo_bib |>
+  select(id, year, displayauth, title, journal) |>
+  left_join(citation_count, by = c("id" = "old"))
+
+ps_top_cites <- short_bib |>
+  filter(journal == "Philosophical Studies") |>
+  filter(year >= 2003, year <= 2007) |>
+  slice_max(order_by = cites, n = 50)
+
+ps_top_cites_all_years <- short_bib |>
+  filter(journal == "Philosophical Studies") |>
+  slice_max(order_by = cites, n = 50)
+
+p <- c(0.9, 0.8, 0.7, 0.5)
+p_names <- map_chr(p, ~paste0("d.", .x*100, "%"))
+p_funs <- map(p, ~partial(quantile, probs = .x, na.rm = TRUE)) |>
+  set_names(nm = p_names)
+citation_deciles <- short_bib |>
+  filter(journal == "Philosophical Studies") |>
+  filter(year >= 1980, year <= 2019) |>
+  group_by(year) |>
+  summarize_at(vars(cites), p_funs) %>%
+  pivot_longer(cols = starts_with("d."), names_to = "decile", names_prefix = "d.", values_to = "cites")
+
+ggplot(citation_deciles, aes(x = year, y = cites, color = decile)) + 
+  geom_point(size = 1.5, alpha = 1.5) +
+  geom_smooth(se = F, method = "loess", formula = 'y ~ x', linewidth = 0.1)
