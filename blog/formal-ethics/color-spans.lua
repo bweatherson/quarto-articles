e9f6457481d175500df0388aeff65b2216c8616d
bweatherson <brian@weatherson.org>
2025-08-29 12:30:18 -0400

    Added color to Nebel post

---
+++ blog/formal-ethics/color-spans.lua
@@ -0,0 +1,12 @@
+function Span(span)
+  if span.classes:includes('text-red') then
+    if FORMAT:match('latex') then
+      return pandoc.RawInline('latex', '\\textcolor{red}{' .. pandoc.utils.stringify(span.content) .. '}')
+    end
+  elseif span.classes:includes('text-blue') then
+    if FORMAT:match('latex') then
+      return pandoc.RawInline('latex', '\\textcolor{blue}{' .. pandoc.utils.stringify(span.content) .. '}')
+    end
+  end
+  return span
+end
